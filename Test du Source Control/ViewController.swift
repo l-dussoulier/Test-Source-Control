0b8ce5b6ee51add90e38d89abc8f7d2b3e962991
DUSSOULIER <dussoulier.louis@hotmail.fr>
2018-07-13 16:21:08 +0200

    revert 2nd commit

--- Test du Source Control/ViewController.swift
+++ Test du Source Control/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("2 nd commit")
+        print("Premier commit")
     }
 
     override func didReceiveMemoryWarning() {
