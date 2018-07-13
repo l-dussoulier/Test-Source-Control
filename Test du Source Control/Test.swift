4d7353daa518a6f6086e56a52c50b2733f7228c2
DUSSOULIER <dussoulier.louis@hotmail.fr>
2018-07-13 16:27:23 +0200

    Ajout classe Test

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by DUSSOULIER on 13/07/2018.
+//  Copyright © 2018 DUSSOULIER. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print("Salut les codeur")
+    }
+}
